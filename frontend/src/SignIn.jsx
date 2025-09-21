cb70465fd12ba1cd63baf759ba1de3d523528c8f
Pavan@42 <pandipavan48@gmail.com>
2025-09-21 15:29:43 +0530

    Roses and Guns

--- frontend/src/SignIn.jsx
+++ frontend/src/SignIn.jsx
@@ -9,16 +9,20 @@ const SignIn = () => {
         <div className="flex items-center  flex-col justify-center mt-10 gap-5  rounded-e-xs">
             <NavBar />
             <div className="w-[90%] flex flex-col justify-center items-center  gap-2">
-                <form className="flex flex-col justify-center border-1 border-black w-1/3 p-9 items-center gap-5 mb-5">
-                    <div className="w-[90%] flex flex-col gap-2">
+                <form className="flex flex-col justify-center border-1 border-black w-[90%] md:w-1/3 p-9 items-center gap-5 mb-5">
+                    <div className="w-[90%]  flex flex-col gap-2">
                         <h1 className="text-2xl text-black-600 justify-center items-center text-center font-bold gap-5 ">Sign In</h1>
                         <p className="text-xl text-gray-600 text-center  justify-center items-center  font-semibold flex">Access Your Account</p>
-                        <p className="text-md text-black font-semibold"> Email-Address</p><div className="relative ">
-                        <input type="text" placeholder="Enter your Email" className="border-1 border-x-black-600 py-4 px-3 w[100%] pl-12 rounded-2xl focus:outline-none focus:border-blue-500">
-                        </input>  <MailPlus className="absolute top-5 ml-3" /></div>
-                        <p className="text-md text-black font-semibold">Password</p><div className="relative">
-                        <input type="text" placeholder="Enter your Password" className="border-1 border-gray-600 py-4 px-3 w[100%]  pl-12 rounded-2xl focus:outline-none focus:border-blue-500">
-                        </input> <UserLock className="absolute top-4 ml-3" /> <Eye className="absolute top-5 ml-3 right-4" /></div>
+                        <p className="text-md text-black font-semibold"> Email-Address</p>
+                        <div className="relative  w-full">
+                            <input type="text" placeholder="Enter your Email" className="border-1 border-x-black-600 py-4 px-3 w-[100%] pl-12 rounded-2xl focus:outline-none focus:border-blue-500">
+                            </input>  <MailPlus className="absolute top-5 ml-3" />
+                        </div>
+                        <p className="text-md text-black font-semibold">Password</p>
+                        <div className="relative">
+                            <input type="text" placeholder="Enter your Password" className="border-1 border-gray-600 py-4 px-3 w-[100%]  pl-12 rounded-2xl focus:outline-none focus:border-blue-500">
+                            </input> <UserLock className="absolute top-4 ml-3" /> <Eye className="absolute top-5 ml-3 right-4" />
+                        </div>
                     </div>
                     <div className="flex gap-2 w-[90%] py-5 px-4 rounded-xl justify-between">
                         <div className="flex gap-2">
--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -1,135 +1,170 @@
-import React from "react";
-import NavBar from "./NavBar";
-import { Link } from "react-router-dom";
-import { UserRoundPen } from 'lucide-react';
-import { UserLock } from 'lucide-react';
-import { EyeOff } from 'lucide-react';
-import { MailPlus } from 'lucide-react';
+import React from 'react'
+import NavBar from './NavBar'
+import { Link } from 'react-router-dom'
+import { CircleUser } from 'lucide-react';
+import { LockKeyhole } from 'lucide-react';
+import { Mail } from 'lucide-react';
+import { FolderPen } from 'lucide-react';
 import { Eye } from 'lucide-react';
-import { useState } from "react";
-
-
+import { EyeOff } from 'lucide-react';
+import { useState } from 'react';
 
 
 const SignUp = () => {
-
-    const [formData, setformData] = useState({
+    const [formData, setFormData] = useState({
         fullName: "",
         email: "",
         password: "",
-        ConfirmPassword: ""
+        confirmPassword: ""
 
     })
-
-
+    const [errors, setErrors] = useState({
+        fullName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
     const [showPassword, setShowPassword] = useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     const [error, setError] = useState('')
     const [success, setSuccess] = useState('')
-
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let newErrors = {}
+        if (!formData.fullName) {
+            newErrors.fullName = "Please enter your full name"
+        }
+        if (!formData.email) {
+            newErrors.email = "Please enter your email"
+        }
+        if (!formData.password) {
+            newErrors.password = "Please enter your password"
+        }
+        if (!formData.confirmPassword) {
+            newErrors.confirmPassword = "Please confirm your password"
+        } else if (formData.password !== formData.confirmPassword) {
+            newErrors.confirmPassword = "Passwords are mismatched"
+        }
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+        } else {
+            setSuccess("Account Created Sucessfully")
+        }
+        setFormData({
+            fullName: "",
+            email: "",
+            password: "",
+            confirmPassword: ""
+        })
+        // if(!formData.fullName || !formData.email || !formData.password || !formData.confirmPassword){
+        //     setError("Please fill all the required fields")
+        // }else if(formData.password !== formData.confirmPassword){
+        //     setError("Your passwords are mismatched")
+        // }else{
+        //     setSuccess("Account Created Successfully")
+        //     setError("")
+        //     setFormData({
+        //         fullName:"",
+        //         email:"",
+        //         password:"",
+        //         confirmPassword:""
+        //     })
+        // }
+    }
     const handlePassword = () => {
-        setShowPassword((password) => !password)
+        setShowPassword((password) => !password);
     }
     const handleConfirmPassword = () => {
-        setShowConfirmPassword((ConfirmPassword) => !ConfirmPassword)
+        setShowConfirmPassword((password) => !password);
     }
     const handleChange = (event) => {
         setError("")
         setSuccess("")
-        setformData((formData) => ({
+        setFormData((formData) => ({
             ...formData,
             [event.target.name]: event.target.value
         }))
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault()
-
-
-        if (!formData.fullName || !formData.email || !formData.password || !formData.ConfirmPassword) {
-            setError("please fill all the details")
-        } else if (formData.password !== formData.ConfirmPassword) {
-            setError("")
-        } else {
-            setSuccess(" Account created successfully")
-            setformData({
-                fullName: "",
-                email: "",
-                password: "",
-                ConfirmPassword: ""
-            })
-        }
 
     }
-
-
-
-
     return (
-        <div className="flex items-center flex-col  gap-5">
-            <NavBar />
-            <h1 className="text-2xl text-blue-600 font-bold gap-5 ">Join BlogVerse</h1>
-            <p className="text-xl text-gray-600 text-center font-semibold">Create your account and <br></br>start your blogging journey today</p>
-            <form onSubmit={handleSubmit} className="flex flex-col border-1 border-black w-1/3 p-7 items-center gap-5 mb-5">
-                <div className="w-[90%] flex flex-col gap-2">
-                    <p className="text-md text-gray-700 font-semibold">Full Name</p>
-                    <input
+        <>
 
-                        value={formData.fullName}
-                        onChange={handleChange}
-                        name="fullName"
-                        type="text" placeholder="Enter Your Full Name" className="border-1 border-gray-600 py-4 px-3  rounded-2xl focus:outline-none focus:border-purple-500">
-                    </input>
-                </div>
-                <div className="w-[90%] flex flex-col gap-2">
-                    <p className="text-md text-gray-700 font-semibold">E-Mail</p><div className="relative">
-                        <input
+            <div className="flex items-center flex-col  gap-5">
+                <NavBar />
+                <h1 className="text-2xl text-blue-600 font-bold">Join BlogVerse</h1>
+                <p className="text-xl text-gray-600 text-center font-semibold">Create your account and <br></br>start your Blogging journey today</p>
+                <form onSubmit={handleSubmit} className=" flex flex-col border-1 border-gray-400 w-[90%] md:w-1/3 py-5 items-center gap-5 rounded-xl">
+                    <div className="w-[90%]  flex flex-col gap-2">
+                        <p className="text-md text-gray-700 font-semibold">Full Name</p>
+                        <div className='relative'>
+                            <input
+                                value={formData.fullName}
+                                onChange={handleChange}
+                                type="text" name="fullName"
+                                placeholder="Enter your Full Name"
+                                className=" pl-12 border-1 border-gray-700 w-full items-center py-2 px-1 rounded-xl focus:outline-none focus:border-purple-500" />
+                            <FolderPen className="absolute top-2 left-3 stroke-gray-500" />
 
-                            value={formData.email}
-                            onChange={handleChange}
-                            name="email"
-                            type="email"
-                            placeholder="Enter Your E-Mail"
-                            className="border-1 border-gray-600 py-4 px-3 w-[100%]  pl-12 rounded-2xl focus:outline-none focus:border-purple-500">
-                        </input><MailPlus className="absolute top-5 ml-3" />
+                        </div>
+                        {errors.fullName && <p className="text-red-500">{errors.fullName}</p>}
                     </div>
-                </div>
-                <div className="w-[90%] flex flex-col gap-2">
-                    <p className="text-md text-gray-700 font-semibold"> Enter Your New Password</p> <div className="relative">
-                        <input
+                    <div className="w-[90%]  flex flex-col gap-2">
+                        <p className="text-md text-gray-700 font-semibold">E-mail</p>
+                        <div className='relative'>
+                            <input
+                                value={formData.email}
+                                onChange={handleChange}
+                                type="email" name="email"
+                                placeholder="Enter your E-mail"
+                                className=" pl-12 border-1 border-gray-700 w-full items-center py-2 px-1 rounded-xl focus:outline-none focus:border-purple-500" />
+                            <Mail className="absolute top-2 left-3  stroke-gray-500" />
 
-                            value={formData.password}
-                            onChange={handleChange}
-                            name="password"
-                            type={showPassword ? "password" : "text"} placeholder="Enter Your New Passwoord" className="border-1 border-gray-600 py-4 px-3   w-[100%]  pl-12 rounded-2xl focus:outline-none focus:border-purple-500">
-                        </input> <UserLock className="absolute top-4 ml-3" /><p onClick={handlePassword}>{showPassword ? <Eye className=" absolute top-4 ml-3 right-4" /> : <EyeOff className=" absolute top-4 ml-3 right-4" />}</p></div>
-                </div>
-                <div className="w-[90%] flex flex-col gap-2">
-                    <p className="text-md text-gray-700 font-semibold">Confirm Your New Password</p> <div className="relative">
-                        <input
-                            value={formData.ConfirmPassword}
-                            onChange={handleChange}
-                            name="ConfirmPassword"
-                            type={showConfirmPassword ? "password" : "text"} placeholder="confirm Your New Passowrd" className="border-1 border-gray-600 py-4 px-3  w-[100%] pl-12  rounded-2xl focus:outline-none focus:border-purple-500">
-                        </input><UserLock className="absolute top-4 ml-3" /><p onClick={handleConfirmPassword}>{showConfirmPassword ? <Eye className=" absolute top-4 ml-3 right-4" /> : <EyeOff className=" absolute top-4 ml-3 right-4" />}</p></div>
-                </div>
-                <div className="flex gap-2 border-1 border-gray-800 w-[90%] py-5 px-4 rounded-xl items-center justify-center">
-                    <input type="checkbox" name="" id="" />
-                    <p>I accept the Terms and Conditions </p>
-                </div>
-                {error && <p className="text-red-600">{error}</p>}
-                {success && <p className="text-green-600">{success}</p>}
-                <button className="w-[90%] bg-purple-500 py-4 text-white rounded xl font-semibold flex justify-center gap-5"> <UserRoundPen /><p>Create Account</p></button>
-                <p className="text-gray-700 font-semibold">Already have an Account?
-                    <Link to="/signin" className="text-purple-500 font-semibold ">Sign In Here</Link>
-                </p>
-                <Link to="/Home" className="text-gray-700 font-semibold hover:bg-gray-200 px-4 w-[90%] rounded-xl cursor-pointer text-center">Back to Home</Link>
-            </form>
-
-        </div>
-
-
+                        </div>
+                        {errors.email && <p className="text-red-500">{errors.email}</p>}
+                    </div>
+                    <div className="w-[90%]  flex flex-col gap-2">
+                        <p className="text-md text-gray-700 font-semibold">Password</p>
+                        <div className='relative'>
+                            <input
+                                value={formData.password}
+                                onChange={handleChange}
+                                type={showPassword ? "text" : "password"}
+                                name="password"
+                                placeholder="Enter your password"
+                                className=" pl-12 border-1 border-gray-700 w-full items-center py-2 px-1 rounded-xl focus:outline-none focus:border-purple-500" />
+                            <LockKeyhole className="absolute top-2 left-3  stroke-gray-500" />
+                            <p onClick={handlePassword}>{showPassword ? <Eye className='absolute right-3 top-2 stroke-gray-500' /> : <EyeOff className="absolute right-3 top-2 stroke-gray-500" />}</p>
+                        </div>
+                        {errors.password && <p className="text-red-500">{errors.password}</p>}
+                    </div>
+                    <div className="w-[90%]  flex flex-col gap-2">
+                        <p className="text-md text-gray-700 font-semibold">Confirm Password</p>
+                        <div className='relative'>
+                            <input
+                                value={formData.confirmPassword}
+                                onChange={handleChange}
+                                type={showConfirmPassword ? "text" : "password"}
+                                name="confirmPassword"
+                                placeholder="Confirm your password"
+                                className="pl-12 border-1 border-gray-700 w-full items-center py-2 px-1 rounded-xl focus:outline-none focus:border-purple-500" />
+                            <LockKeyhole className="absolute top-2 left-3  stroke-gray-500" />
+                            <p onClick={handleConfirmPassword}>{showConfirmPassword ? <Eye className='absolute right-3 top-2 stroke-gray-500' /> : <EyeOff className="absolute right-3 top-2 stroke-gray-500" />}</p>
+                        </div>
+                        {errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword}</p>}
+                    </div>
+                    <div className="flex gap-2 border-1 border-gray-800 w-[90%] py-2 px-4 rounded-xl items-center justify-center">
+                        <input type="checkbox" name="" id="" className="h-5 w-5" />
+                        <p>I agree to the Terms of Service and Privacy Policy</p>
+                    </div>
+                    {error && <p class="text-red-500">{error}</p>}
+                    {success && <p class="text-green-500">{success}</p>}
+                    <button type="submit" className="w-[90%] bg-purple-500 py-3 text-white font-semibold rounded-xl cursor-pointer flex justify-center items-center gap-2"> <CircleUser className="" /> <p>Create Account</p></button>
+                    <div className="border-[0.5px] border-gray-400 w-[90%] mt-4"></div>
+                    <p className="text-gray-700 font-semibold">Aldready have an Account?<Link to="/signin" className="text-purple-500 font-semibold">Sign In Here</Link></p>
+                    <Link to="/home" className="text-gray-700 font-semibold py-3 w-[90%] rounded-xl cursor-pointer text-center hover:bg-gray-200">Back to Home</Link>
+                </form>
+            </div>
+        </>
     )
 }
-
 export default SignUp
